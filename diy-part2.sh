cb7b53ce739024249f1111acbfbaab67eccd7804
hibuddies <odpazi04973@gmail.com>
2020-12-18 21:53:05 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# theme
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy package/luci-theme-rosy
