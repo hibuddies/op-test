f2bd4e1d8c378260ed8ae8bc8d57a092d2639096
hibuddies <odpazi04973@gmail.com>
2020-12-24 15:49:33 +0800

    Update autoVersion.sh

--- ARMv8/autoVersion.sh
+++ ARMv8/autoVersion.sh
@@ -11,11 +11,12 @@ sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_n1_opimg_s
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_s905x3_opimg.sh
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_s905x3_opimg_sfe.sh
 
+# 修改sfe打包脚本内底包名字
 sed -i "s/OPWRT_ROOTFS_GZ=.*/OPWRT_ROOTFS_GZ=\"$\{PWD}\/sfe-\openwrt-armvirt-64-default-rootfs.tar.gz\"/" /opt/openwrt/mk_beikeyun_opimg_sfe.sh
 sed -i "s/OPWRT_ROOTFS_GZ=.*/OPWRT_ROOTFS_GZ=\"$\{PWD}\/sfe-\openwrt-armvirt-64-default-rootfs.tar.gz\"/" /opt/openwrt/mk_n1_opimg_sfe.sh
 sed -i "s/OPWRT_ROOTFS_GZ=.*/OPWRT_ROOTFS_GZ=\"$\{PWD}\/sfe-\openwrt-armvirt-64-default-rootfs.tar.gz\"/" /opt/openwrt/mk_s905x3_opimg_sfe.sh
 
-# 修改sfe打包脚本内核版本
+# 修改sfe打包脚本内的内核版本
 sed -i "s/KERNEL_VERSION=.*/KERNEL_VERSION=\"5.9.14-flippy-50+\"/" /opt/openwrt/mk_beikeyun_opimg_sfe.sh
 sed -i "s/KERNEL_VERSION=.*/KERNEL_VERSION=\"5.9.14-flippy-50+\"/" /opt/openwrt/mk_n1_opimg_sfe.sh
 sed -i "s/KERNEL_VERSION=.*/KERNEL_VERSION=\"5.9.14-flippy-50+\"/" /opt/openwrt/mk_s905x3_opimg_sfe.sh
